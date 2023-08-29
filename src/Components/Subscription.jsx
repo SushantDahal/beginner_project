ff83b8f75db78ed0e6f4fb645774a2a38b1a972e
SushantDahal <SushanDahal246@gmail.com>
2023-08-29 16:16:23 +0545

    Hover Effect

--- src/Components/Subscription.jsx
+++ src/Components/Subscription.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Subscription(props) {
   return (
-    <div className="w-full shadow-xl flex flex-col py-4 my-4  rounded-lg">
+    <div className="w-full shadow-xl flex flex-col py-4 my-4  rounded-lg hover:scale-110 duration-500">
       <img className="w-20 mx-auto mt-[-3rem]" src={props.img} alt="/" />
       <h2 className="text-center text-2xl py-8 font-bold">{props.user}</h2>
       <p className="text-center text-4xl font-bold">{props.price}</p>
@@ -11,7 +11,7 @@ function Subscription(props) {
         <p className="border-b py-4 mx-8">{props.grantedUser}</p>
         <p className="border-b py-4 mx-8">{props.sendUpto}</p>
       </div>
-      <button className="bg-black w-[140px] mx-auto text-white rounded my-4 py-2">
+      <button className="bg-black w-[140px] mx-auto text-[#00df9a] rounded my-4 py-2">
         {props.btn}
       </button>
     </div>
