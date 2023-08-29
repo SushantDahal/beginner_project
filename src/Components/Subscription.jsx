15f79be8f77968650d858135f60c2d31315f3df2
SushantDahal <SushanDahal246@gmail.com>
2023-08-29 16:09:45 +0545

    Fixing image error

--- src/Components/Subscription.jsx
+++ src/Components/Subscription.jsx
@@ -7,9 +7,9 @@ function Subscription(props) {
       <h2 className="text-center text-2xl py-8 font-bold">{props.user}</h2>
       <p className="text-center text-4xl font-bold">{props.price}</p>
       <div className="text-center font-medium">
-        <p className="border-b py-2 mx-8 mt-8">{props.storage}</p>
-        <p className="border-b py-2 mx-8">{props.grantedUser}</p>
-        <p className="border-b py-2 mx-8">{props.sendUpto}</p>
+        <p className="border-b py-4 mx-8 mt-8">{props.storage}</p>
+        <p className="border-b py-4 mx-8">{props.grantedUser}</p>
+        <p className="border-b py-4 mx-8">{props.sendUpto}</p>
       </div>
       <button className="bg-black w-[140px] mx-auto text-white rounded my-4 py-2">
         {props.btn}
--- src/card.js
+++ src/card.js
@@ -1,7 +1,7 @@
 const cardComponents = [
   {
     id: "1",
-    img: "single.png",
+    img: require("./assets/single.png"),
     user: "Single User",
     price: "$150",
     storage: "500 GB Storage",
@@ -11,7 +11,7 @@ const cardComponents = [
   },
   {
     id: "2",
-    img: "double.png",
+    img: require("./assets/double.png"),
     user: "Double User",
     price: "$199",
     storage: "9000 GB Storage",
@@ -21,7 +21,7 @@ const cardComponents = [
   },
   {
     id: "3",
-    img: "Triple.png",
+    img: require("./assets/triple.png"),
     user: "Triple User",
     price: "$250",
     storage: "1600 GB Storage",
